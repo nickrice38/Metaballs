b88634298b57f061c5dfdde2e15537f64497a45d
Nick Rice <nick.rice@starlingbank.com>
2022-09-22 11:06:53 +0100

    Initial Commit

---
+++ Metaballs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Metaballs
+//
+//  Created by Nick Rice on 22/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metaballs/MetaballsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaballsApp.swift
+//  Metaballs
+//
+//  Created by Nick Rice on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MetaballsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
